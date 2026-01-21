52e5d5e80083fb70d29988b732113fea659b1fc0
EduardoTorresAlamo <torreseduardo804@gmail.com>
2026-01-21 17:16:13 -0400

    Initial Commit

---
+++ PocketPay/PocketPayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketPayApp.swift
+//  PocketPay
+//
+//  Created by Eduardo Torres on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct PocketPayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
